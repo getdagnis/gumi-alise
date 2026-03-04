d8395e7dcc55c87ae00682169a31260d5c63fd5d
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2026-03-04 19:47:57 +0200

    bugfix/global styles emitted in production fix attempt 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/globals.module.scss';
+import './styles/globals.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
